6de70a461e5b8f560c1cb5a0766fdbd369fb719a
curciog <curciogdev@gmail.com>
2026-09-07 16:48:43 -0300

    Aula 04: Verificar tamanho e imprimir elementos do vetor

--- src/com/gabriel/estruturadados/vetor/Vetor.java
+++ src/com/gabriel/estruturadados/vetor/Vetor.java
@@ -1,15 +1,17 @@
 package com.gabriel.estruturadados.vetor;
 
+import java.util.Arrays;
+
 public class Vetor {
 
 	private String[] elementos;
 	private int tamanho;
-	
+
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
 		this.tamanho = 0;
 	}
-	
+
 	/*public void adiciona(String elemento) {
 		for (int i = 0; i < this.elementos.length; i++) {
 			if (this.elementos[i] == null) {
@@ -18,27 +20,51 @@ public class Vetor {
 			}
 		}
 	}*/
-	
+
 	/*public void adiciona(String elemento) throws Exception {
-		
+
 		if (this.tamanho < this.elementos.length) {
 			this.elementos[this.tamanho] = elemento;
 			this.tamanho++;
 		} else {
 			throw new Exception("Vetor já está cheio, não é possível adicionar mais elementos.");
 		}
-		
+
 		this.elementos[this.tamanho] = elemento;
 		this.tamanho++;
 	}*/
-	
-public boolean adiciona(String elemento) {
-		
+
+	public boolean adiciona(String elemento) {
+
 		if (this.tamanho < this.elementos.length) {
 			this.elementos[this.tamanho] = elemento;
 			this.tamanho++;
 			return true;
 		} 
 		return false;
+	}
+
+	public int tamanho() {
+		return this.tamanho;
+	}
+
+	@Override
+	public String toString() {
+		
+		StringBuilder s = new StringBuilder();
+		s.append("[");
+		
+		for (int i = 0; i < this.tamanho - 1; i++) {
+			s.append(this.elementos[i]);
+			s.append(", ");
+		}
+		
+		if (this.tamanho > 0) {
+			s.append(this.elementos[this.tamanho - 1]);
+		}
+		
+		s.append("]");
+		
+		return s.toString();
 	}
 }
--- src/com/gabriel/estruturadados/vetor/teste/Aula04.java
+++ src/com/gabriel/estruturadados/vetor/teste/Aula04.java
@@ -2,14 +2,19 @@ package com.gabriel.estruturadados.vetor.teste;
 
 import com.gabriel.estruturadados.vetor.Vetor;
 
-public class Aula03 {
+public class Aula04 {
 
 	public static void main(String[] args) {
-
-		Vetor vetor = new Vetor(2);
+		
+		Vetor vetor = new Vetor(10);
 
 		vetor.adiciona("elemento 1");
 		vetor.adiciona("elemento 2");
 		vetor.adiciona("elemento 3");
+		
+		System.out.println(vetor.tamanho());
+		
+		System.out.println(vetor);
 	}
+
 }
